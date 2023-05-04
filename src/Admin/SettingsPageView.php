ecbbc957db29fcb57dce35b44e7625f58496f825
Gregory Collin <gregory@d4rk0s.net>
2023-05-04 12:59:00 +0200

    Activate cors functionnality

--- src/Admin/SettingsPageView.php
+++ src/Admin/SettingsPageView.php
@@ -9,6 +9,10 @@
                 <th scope="row">API Namespace</th>
                 <td><input type="text" name="<?php echo \Hyperion\RestAPI\Plugin::API_NAMESPACE_OPTION; ?>" value="<?php echo esc_attr( get_option(\Hyperion\RestAPI\Plugin::API_NAMESPACE_OPTION) ); ?>" /></td>
             </tr>
+            <tr>
+                <th scope="row">Domaine d'origine (activation CORS)</th>
+                <td><input type="text" placeholder="séparez les domaines par une , si vous en avez plusieurs" name="<?php echo \Hyperion\RestAPI\Plugin::API_ORIGIN_CORS_OPTION; ?>" value="<?php echo esc_attr( get_option(\Hyperion\RestAPI\Plugin::API_ORIGIN_CORS_OPTION) ); ?>" /></td>
+            </tr>
         </table>
 
         <?php submit_button(); ?>
--- src/Plugin.php
+++ src/Plugin.php
@@ -5,16 +5,33 @@ namespace Hyperion\RestAPI;
 class Plugin
 {
     public const API_NAMESPACE_OPTION = 'hyperion_api_namespace';
+    public const API_ORIGIN_CORS_OPTION = 'hyperion_api_cors';
     public const ADD_API_ENDPOINT_ACTION = 'add_api_endpoint';
 
     public static function install()
     {
         add_option(self::API_NAMESPACE_OPTION);
+        add_option(self::API_ORIGIN_CORS_OPTION);
     }
 
     public static function uninstall()
     {
         // Remove option from wordpress option
         delete_option(self::API_NAMESPACE_OPTION);
+        delete_option(self::API_ORIGIN_CORS_OPTION);
+    }
+
+    public static function activeCors($value)
+    {
+        $origin = get_http_origin();
+        $allowed_origins = explode(",",get_option(\Hyperion\RestAPI\Plugin::API_ORIGIN_CORS_OPTION));
+
+        if ( $origin && in_array($origin, $allowed_origins, true)) {
+            header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
+            header( 'Access-Control-Allow-Methods: GET,POST' );
+            header( 'Access-Control-Allow-Credentials: true' );
+        }
+
+        return $value;
     }
 }
